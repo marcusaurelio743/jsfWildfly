0dfcaba74ce7c72e4cccd6e3dff5fc8550e8eb42
Marcus Aurélio M. O. Santos <90445199+marcusaurelio743@users.noreply.github.com>
2026-03-10 20:47:32 -0300

    apesar dos meus esforços tive que configurar para o tomcat pois a configuração para funcionar como quero teria que dedicar mais tempo e quero só revisar meus conceitos de jsf

---
+++ pos-java-hibernate/src/main/java/posjava/managebean/UsuarioPessoaBean.java
@@ -0,0 +1,25 @@
+package posjava.managebean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import posjava.dao.DaoGeneric;
+import posjava.model.UsuarioPessoa;
+
+@ManagedBean(name = "usuarioPessoaBean")
+@ViewScoped
+public class UsuarioPessoaBean  {
+	
+	
+	private UsuarioPessoa pessoa = new UsuarioPessoa();
+	private DaoGeneric<UsuarioPessoa> dao = new DaoGeneric<>();
+
+	public void setPessoa(UsuarioPessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	public UsuarioPessoa getPessoa() {
+		return pessoa;
+	}
+
+}
